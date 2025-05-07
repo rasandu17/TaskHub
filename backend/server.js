84c25c8fa1545ba085f9d82ef72c8447f0cfca3a
Ravindu Samaraweera <155237294+rasandu17@users.noreply.github.com>
2025-05-07 17:05:43 +0530

    Add server and environment configuration files

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require ('cors');
+require('dotnev').config();
+
+const authroute = require('./routes/authRoutes')
+const taskroute = require('./routes/taskRoutes')
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('./api/auth', authroute);
+app.use('./api/task', taskroute);
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        app.listen(process.env.PORT,()=>{
+            console.log("Server running on port ${process.env.PORT}")
+
+        });
+    })
+    .catch(err=> console.log(err))
